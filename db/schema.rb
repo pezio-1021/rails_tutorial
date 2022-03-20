75f12610f8e9ad43485b021d246d560b02a90b35
shohei.takahashi <iloveoneokbaske@gmail.com>
2022-03-20 22:07:19 +0900

    add: insert user

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_03_18_140834) do
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
